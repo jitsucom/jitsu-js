939fb41c188df4e425cc4bef286e22a3fa41b080
Vladimir Klimontovich <vladimir@jitsu.com>
2022-06-03 10:36:01 -0400

    Fixed issue with React 18

--- packages/react/src/JitsuProvider.tsx
+++ packages/react/src/JitsuProvider.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import JitsuContext from './JitsuContext'
 import { JitsuClient } from "@jitsu/sdk-js";
+import {PropsWithChildren} from "react";
 
 export interface JitsuProviderProps {
   client: JitsuClient
 }
 
-const JitsuProvider: React.FC<JitsuProviderProps> = function ({children, client}) {
+const JitsuProvider: React.FC<PropsWithChildren<JitsuProviderProps>> = function ({children, client}) {
   const Context = JitsuContext
   return <Context.Provider value={client}>{children}</Context.Provider>
 }
